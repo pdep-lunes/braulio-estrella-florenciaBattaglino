3aa23e2db6da87842a88b0f5a9e41e6841ec670d
florenciaBattaglino <132497929+florenciaBattaglino@users.noreply.github.com>
2025-04-28 12:17:41 -0300

    hecho con jd

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,28 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
+data Personaje = UnPersonaje {
+    nombre :: String,
+    poderBasico :: Poder,
+    superPoder :: Poder,
+    superActivo :: Bool,
+    cantidadVida :: Int }
+
+type Poder = Personaje -> Personaje
+
+espinosa :: Personaje
+espinosa = UnPersonaje "Espinosa" bolaEspinosa bolaEspinosa True 4800
+
+modificarVida :: Personaje -> Int -> Personaje
+modificarVida personaje vida = personaje {cantidadVida = vida}
+
+bolaEspinosa :: Poder
+bolaEspinosa contrincante = modificarVida contrincante (max (cantidadVida contrincante - 1000) 0)
+  
+
+lluviaDeTuercas :: String -> Poder
+lluviaDeTuercas team personaje 
+  | equipo == "colega" = modificarVida personaje (cantidadVida personaje + 800)
+  | equipo == "contrincante" = modificarVida personaje (div (cantidadVida personaje) 2)
+  | otherwise = personaje
+
